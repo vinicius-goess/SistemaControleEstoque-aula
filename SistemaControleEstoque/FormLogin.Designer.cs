9f1ad7b691a3e477426165d62045407b17346025
vinicius-goess <viny_lolo@hotmail.com>
2025-10-01 16:58:42 -0300

    AcceptButton

--- SistemaControleEstoque/FormLogin.Designer.cs
+++ SistemaControleEstoque/FormLogin.Designer.cs
@@ -109,6 +109,7 @@ namespace SistemaControleEstoque
             // 
             // FormLogin
             // 
+            this.AcceptButton = this.btnEntrar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(416, 509);
